417b3d86c7c5779f583313f1f363e138a1bd0c7b
Gull 2365 <jych2365@gmail.com>
2023-08-14 01:54:36 +0900

    feat: useState & reference data type(array)

--- app/list/page.js
+++ app/list/page.js
@@ -1,5 +1,10 @@
+"use client";
+
+import { useState } from "react";
+
 export default function List() {
   let 상품 = ["Tomatoes", "Pasta", "Coconet"];
+  let [수량, 수량변경] = useState([0, 0, 0]);
   return (
     <div>
       <h4 className="title">상품목록</h4>
@@ -8,6 +13,25 @@ export default function List() {
           <div className="food" key={i}>
             <img src={`/food${i}.png`} className="food-img" />
             <h4>{`상품${a} $40`}</h4>
+            <span>{수량[i]}</span>
+            <button
+              onClick={() => {
+                let copy = [...수량];
+                copy[i]++;
+                수량변경(copy);
+              }}
+            >
+              +
+            </button>
+            <button
+              onClick={() => {
+                let copy = [...수량];
+                copy[i]--;
+                수량변경(copy);
+              }}
+            >
+              -
+            </button>
           </div>
         );
       })}
